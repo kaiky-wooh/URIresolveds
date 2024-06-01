96381177fa3205005f41fe25d7dc02fc220b8d82
Kaiky <kaikymoura.dev@outlook.com.br>
2024-05-31 23:44:04 -0300

    first commit

---
+++ areaDoCirculo/areaDoCirculo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+class URI 
+{
+    static void Main(string[] args)
+    {
+        /*
+         --DESAFIO--
+
+        Entrada
+        A entrada contém um valor de ponto flutuante (dupla precisão), 
+        no caso, a variável raio.
+
+        Saida
+        Apresentar a mensagem "A=" seguido pelo valor da variável area, 
+        conforme exemplo abaixo, com 4 casas após o ponto decimal.
+
+         */
+
+        double raio = double.Parse(Console.ReadLine());
+        double area = 3.14159 * (raio * raio);
+
+        Console.WriteLine("A=" + area.ToString("F4"));
+    }
+}
---
+++ extremamenteBasico/extremamenteBasico/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        /*
+         ---DESAFIO---
+
+        - Ler 2 valores inteiros e armazenar em variaveis (A e B)
+        - Efetuar uma soma de A e B atribuindo o resultado na variavel X
+        - Imprimir X (letra maiúscula) seguido pelo valor da variável X
+        - Ter um espaço antes e depois do sinal de igualdade
+         
+         */
+
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        int X = A + B;
+
+        Console.WriteLine("X = " + X);
+    }
+}
